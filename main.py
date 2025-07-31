bcf3d9eb6ad2ae751d5c4e62456e7d4a4425e7d3
Mark Heines <friteuse@arcor.de>
2025-07-31 08:10:14 +0200

    add new method, requirements.tex

--- main.py
+++ main.py
@@ -31,36 +31,74 @@ class onlinebrief24API:
         self.api_secret = os.getenv('API_SECRET')
         self.base_url = "https://api.onlinebrief24.de/v1"
         self.headers = { 'Content-Type': 'application/json' }
-        self.payload = {
+        self.payload_auth = {
             'auth': {
                 'apiKey': f'{self.api_key}',
                 'apiSecret': f'{self.api_secret}',
-                'mode': 'test'
+                'mode': 'live'
                 }
         }
 
     def balance(self):
         url = self.base_url + '/balance'
-        response = requests.get(url, headers=self.headers, json=self.payload)
+        response = requests.get(url, headers=self.headers, json=self.payload_auth)
         return f'Balance: {response.json()['data']['balance']} EUR'
 
     def invoices(self):
         url = self.base_url + '/invoices'
-        response = requests.get(url, headers=self.headers, json=self.payload)
-        return response.json()['data']['invoices']
+        response = requests.get(url, headers=self.headers, json=self.payload_auth)
+        invoices = response.json()['data']['invoices']
+        print('There are currently {len(invoices} invoices.')
+        for inv in invoices:
+            print(f'id: {inv['id']}, invoice date: {inv['invoice_date'].split(' ')[0]}')
+        #return response.json()['data']['invoices']
 
     def get_invoice(self, invoice_id):
         url = self.base_url + f'/invoices/{invoice_id}'
-        response = requests.get(url, headers=self.headers, json=self.payload)
+        response = requests.get(url, headers=self.headers, json=self.payload_auth)
         invoice_date = response.json()['data']['invoice_date'].split(' ')[0]
         base64_data = response.json()['data']['base64_data']
         pdf_data = base64.b64decode(base64_data)
         pdf_filename = f"ob24-Rechnung-{invoice_date}.pdf"
         with open(pdf_filename, "wb") as file:
             file.write(pdf_data)
-            print(f'successfully saved {pdf_filename}')
-        return response.json()
+            print(f'✅ Invoice successfully saved: {pdf_filename}')
+        #return response.json()
+    
+    def open_pdf(self, pdf_filename):
+        with open(f'{pdf_filename}', 'rb') as file:
+            pdf_data = file.read()
+        return pdf_data
 
+    def base64_encode(self, pdf_data):
+        base64_encoded = base64.b64encode(pdf_data).decode("utf8")
+        return base64_encoded
+
+    def md5_checksum(self, base64_data):
+        md5_checksum = hashlib.md5(base64_data.encode('utf-8')).hexdigest()
+        return md5_checksum
+
+    def send_letter(self, pdf_filename):
+        pdf_data = self.open_pdf(pdf_filename)
+        base64_encoded = self.base64_encode(pdf_data)
+        md5_checksum = self.md5_checksum(base64_encoded)
+
+        payload = {
+            **self.payload_auth,
+            "letter": {
+                "base64_file": base64_encoded,
+                "base64_file_checksum": md5_checksum,
+                "filename_original": os.path.basename(pdf_filename),
+                "specification": {
+                    "color": "1",
+                    "mode": "simplex",
+                    "shipping": "auto"
+                }
+            }
+        }        
+        response = requests.post(f"{self.base_url}/printjobs", headers=self.headers, json=payload)
+
+        return response
 
 if __name__ == '__main__':
     ob = onlinebrief24API()
